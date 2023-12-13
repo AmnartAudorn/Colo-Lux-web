98efda33e625e6a85e675d79a1bc6a660ec2b3d7
amnartaud <amnartaud@gosoft.co.th>
2023-12-13 16:28:19 +0700

    Add About us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './page/index/index.component';
+import { AboutUsComponent } from './page/about-us/about-us.component';
 
-const routes: Routes = [{ path: '', component: IndexComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: IndexComponent },
+  { path: 'about', component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './page/index/index.component';
+import { NavberComponent } from './page/navber/navber.component';
+import { FooterComponent } from './page/footer/footer.component';
+import { AboutUsComponent } from './page/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    NavberComponent,
+    FooterComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
